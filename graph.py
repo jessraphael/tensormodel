c3dca9adde1882e57d9ba2014236e447d8fcce0e
Jess Raphael <jxssraphael11@gmail.com>
2021-10-17 03:43:11 +0800

    UI
    UI

--- graph.py
+++ graph.py
@@ -7,38 +7,42 @@ from darts import TimeSeries
 import numpy as np
 
 ## insert json file path here
-file_path = "FILE_PATH_HERE"
+def get_time_diff(file_path):
 
-with open(file_path) as f:
+    with open(file_path) as f:
         data = json.loads(f.read())
         msgs = data['messages']
 
-df = pd.DataFrame(msgs)
+    df = pd.DataFrame(msgs)
 
-df_new = df.filter(items= ["from", "date"])
-df_new = df_new.sort_values(by="date")
-
-
-print(df_new)
-
-def getTimeDiff(df):
-    output = []
+    df_new = df.filter(items= ["from", "date"])
+    df_new = df_new.sort_values(by="date")
 
 
-    for x in range(0, len(df)):
-        if x != 0 and df.iloc[x][0] != df.iloc[x-1][0]:
-            username = df.iloc[x][0]
-            timeDiff = (dateutil.parser.isoparse(df.iloc[x][1]).timestamp()) - (dateutil.parser.isoparse(df.iloc[x-1][1]).timestamp())
-            output.append(timeDiff)
-    return output
+    print(df_new)
 
-y= getTimeDiff(df_new)
-x= list(range(1,len(y)+1))
+    def getTimeDiff(df):
+        output = []
 
-print(x)
-print(y)
-average_waiting_time =  sum(y)/len(x)
 
-plt.plot(x,y)
-plt.title("Response rate vs number of texting instances")
-plt.show()
+        for x in range(0, len(df)):
+            if x != 0 and df.iloc[x][0] != df.iloc[x-1][0]:
+                username = df.iloc[x][0]
+                timeDiff = (dateutil.parser.isoparse(df.iloc[x][1]).timestamp()) - (dateutil.parser.isoparse(df.iloc[x-1][1]).timestamp())
+                output.append(timeDiff)
+        return output
+
+    y= getTimeDiff(df_new)
+
+    return y
+
+def get_number_of_instances(y):
+    return list(range(1,len(y)+1))
+
+def get_average_waiting_time_in_seconds(x,y):    
+    return sum(y)/len(x)
+
+def plot_graph(x,y):
+    plt.plot(x,y)
+    plt.title("Response rate vs number of texting instances")
+    return plt.show()
--- individual.py
+++ individual.py
@@ -6,23 +6,30 @@ import pandas as pd
 from darts import TimeSeries
 import numpy as np
 
-## insert json file path here
-file_path = "yijie3.json"
+# insert json file path here
 
-with open(file_path) as f:
+def get_individual_statistics(file): #filepath here
+    file_path = file 
+
+    with open(file_path) as f:
         data = json.loads(f.read())
         msgs = data['messages']
 
-df = pd.DataFrame(msgs)
+    df = pd.DataFrame(msgs)
 
-df_new = df.filter(items= ["from", "date"])
-df_new = df_new.sort_values(by="date")
+    df_new = df.filter(items=["from", "date"])
+    df_new = df_new.sort_values(by="date")
 
 
-def get_total_messages_sent(df):
-    df_new = df
-    df_new = df_new.groupby(by="from").count()
-    print (df_new)
-    return df_new
-    
-get_total_messages_sent(df_new)
+    def get_total_messages_sent(df):
+        df_new = df
+        df_new = df_new.groupby(by="from").count()
+        print(df_new)
+        return df_new
+
+
+    return get_total_messages_sent(df_new)
+
+
+
+get_individual_statistics("yijie3.json")
